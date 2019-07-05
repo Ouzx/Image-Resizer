a7754172cf6075f3f1d3b4da8e5b170e88154e28
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2019-07-05 22:28:28 +0300

    V1

---
+++ WindowsFormsApp26/WindowsFormsApp26/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApp26
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(397, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Aç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Konum:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(397, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Başlat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dosya Sayısı: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 118);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Hedef Konum:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 143);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Image Resizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp26/WindowsFormsApp26/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp26
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const int WIDTH = 800; //En
+        const int HEIGHT = 600; //Boy
+
+        string filepath = ""; // Kaynak Dosya Yolu
+        string destpath =""; //Hedef Dosya Yolu
+        int fileCounter = 0; //Klasördeki Dosya Sayacı
+        int progress = 0; //İlerleme(değiştirilen dosya sayısı)
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo di = new DirectoryInfo(filepath);
+            FileInfo[] fi = di.GetFiles();
+
+            //Klasördeki tüm dosyaları dolaşır.
+            for(int i = 0; i < fi.Length; i++)
+            {
+                string tempPath = filepath +"\\"+ fi[i].Name; //Sıradaki dosyanın dosya yolu.
+
+                //çözünürlüğü değiştirilmiş resim
+                Bitmap changed = new Bitmap(new Bitmap(tempPath), WIDTH, HEIGHT); //Çöznürlüğü değiştirilmiş resim.
+                progress++;
+                label2.Text = "İşlenen Dosya Sayısı: " + progress.ToString();
+                changed.Save(destpath + "\\" + progress.ToString() + ".png");
+
+                //Değişkenleri boşalt.
+                fi[i] = null;
+                changed = null;
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            MessageBox.Show("Kaynak Dosya Yolunu Seçiniz:");
+            fbd.ShowDialog();
+            filepath = fbd.SelectedPath;
+            label1.Text = filepath;
+
+            MessageBox.Show("Hedef Dosya Yolunu Seçiniz:");
+            fbd.ShowDialog();
+            destpath = fbd.SelectedPath;
+            label4.Text = "Hedef Dosya Konumu: " + destpath;
+
+            DirectoryInfo di = new DirectoryInfo(filepath);
+            FileInfo[] fi = di.GetFiles();
+            fileCounter = fi.Length;
+            label2.Text = "Dosya Sayısı: " + fileCounter.ToString();
+
+            fi = null;
+        }
+
+
+    }
+}
